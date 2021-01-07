2ae0bb11e1bc8181c3c167d73ac036fd5c11dfec
Joshua Hruzik <Joshua.Hruzik@gmail.com>
2021-01-07 21:03:10 +0100

    Add files via upload
    LCD Display to show message. Missing pause between signals included.

--- README.md
+++ README.md
@@ -1,33 +1,38 @@
 # Telegraphy
 
-This project will transform any String message into Morse code that will be played by a buzzer connected to a Raspberry Pi.
+This project will transform any String message into Morse code that will be played by a buzzer connected to a Raspberry Pi. Also, the Message will be played to a small LCD.
 
 ## Setup
 In order to recreate my working setup you will need the following setup:
 * 1x Raspberry Pi
 * 1x Active Buzzer
-* 4x Jumper Cables
+* 8x Jumper Cables
 * 1x 1K Ohm Resistor
 * 1x 220 Ohm Resistor
 * 1x NPN Transistor
+* 1x I2C LCD1602
 
 The wiring should look like this:
 ![Sketch](https://raw.githubusercontent.com/Jhruzik/Telegraphy/main/Sketch.png)
 
-Note that the 220 Ohm resistor between 5V and the active buzzer is optional. I included it to decrease the buzzer's volume.
+Note that the 220 Ohm resistor between 5V and the active buzzer is optional. I included it to decrease the buzzer's volume. 
+
+In this sketch you don't actually see an LCD. Instead an Adafruit Char LCD is connected to an I2C pcf8574 adapter which is hooked up to the Raspberry Pi.
 
 Make sure to install RPi.GPIO to access your Raspberry Pi's GPIO pins from Python:
 ```
 pip install RPi.GPIO
 ```
 
+Also, you need the drivers for the LCD in the same folder where this script is located at. You can download the drivers from [Freenove's Github Repo](https://github.com/Freenove/Freenove_Ultimate_Starter_Kit_for_Raspberry_Pi/tree/master/Code/Python_Code/20.1.1_I2CLCD1602). Make sure to download the driver for [PCF8574](https://raw.githubusercontent.com/Freenove/Freenove_Ultimate_Starter_Kit_for_Raspberry_Pi/master/Code/Python_Code/20.1.1_I2CLCD1602/PCF8574.py) and the [Adafruit Char LCD] (https://raw.githubusercontent.com/Freenove/Freenove_Ultimate_Starter_Kit_for_Raspberry_Pi/master/Code/Python_Code/20.1.1_I2CLCD1602/Adafruit_LCD1602.py). Download and place these files wherever this script is located at.
+
 ## CLI
 After recreating the setup and installing GPi.GPIO, you can use the command line like so:
 ```
-python telegraphy.py 11 "Hello World"
+python telegraphy.py 11 3 "Hello World"
 ```
 
-The first argument must always be the physical pin location you want to use to emit the electrical signal. In my case, I use GPIO17 which is equal to pin 11 in my setup. The second argument will be the message to morse in double quotes. Notice that your message must only contain letters that are also present in the Morse alphabet. I included Umlauts to also render German messages.
+The first argument must always be the physical pin location you want to use to emit the electrical signal for the buzzer for. In my case, I use GPIO17 which is equal to pin 11 in my setup. The second argument is the SDA pin for the pcf adapter. In my setup it is pin 3. Finally, the third argument will be the message to morse in double quotes. Notice that your message must only contain letters that are also present in the Morse alphabet. You can also use Umlauts since they will be translated into the English counter part.
 
 ## Notice
 I am by no means a professional electrician. Hence, I take no responsibility for any damage that may result. This is a hobby project done to learn about the Raspberry Pi and electricity.
--- telegraphy.py
+++ telegraphy.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 import re
 import argparse
 from time import sleep
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
 
 
 # Define Morse Alphabet
@@ -18,37 +20,23 @@ morse_code_dict = { 'A':'.-', 'B':'-...',
                     '1':'.----', '2':'..---', '3':'...--',
                     '4':'....-', '5':'.....', '6':'-....',
                     '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '0':'-----', ',':'--..--', '.':'.-.-.-',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-', ' ': ' ',
-                    'Ä':'.-.', 'Ö':'---.', 'Ü':'..-.'}
+                    '(':'-.--.', ')':'-.--.-', ' ': ' '} 
 
 
-# Translate Message to Morse Alphabet
-def translate(msg: str) -> str:
-
-    # Normalize Message
-    msg_clean = re.sub("\s+", " ", msg)
-    msg_clean = msg_clean.upper()
-
-    # Check for Non-Existing Characters
-    msg_dict = set(msg_clean)
-    unkown_chars = msg_dict - set(morse_code_dict.keys())
-    if len(unkown_chars) > 0:
-        raise ValueError("The following characters don't exist in the Morse Alphabet: ['{}']".format("', '".join(unkown_chars)))
-
-    # Code Message
-    msg_coded = []
-    for letter in msg_clean:
-        msg_coded.append(morse_code_dict[letter])
-    msg_coded = "".join(msg_coded)
-
-    # Return Coded Message
-    return msg_coded
+# Check Message
+def check_message(msg:str, alphabet: set) -> bool:
+    letters = set(msg.upper())
+    return len(letters-alphabet) == 0
 
 
-# Play Message
-def play(pin: int, msg: str):
+# Play Tone
+def play(pin: int, tone: str):
+
+    # Check User Input
+    if tone not in [".", "-"]:
+        raise ValueError("Tone must be either '.' or '-'")
 
     # Function to Play Tone
     def _beep(pin: int, t: float):
@@ -64,23 +52,50 @@ def play(pin: int, msg: str):
     def _long(pin: int):
         _beep(pin, 0.3)
 
-    # Play Message
-    pause = 0.3
-    for tone in msg:
-        if tone == '.':
-            _short(pin)
-            sleep(pause)
-        elif tone == "-":
-            _long(pin)
-            sleep(pause)
-        elif tone == " ":
-            sleep(0.7)
+    # Play Tone
+    pause = 0.1
+    if tone == '.':
+        _short(pin)
+        sleep(pause)
+    elif tone == "-":
+        _long(pin)
+        sleep(pause)
+
+# Show Letter
+def show(lcd, letter: str, line: int, column: int):
+    lcd.setCursor(column, line)
+    lcd.message(letter)
 
 
 # Setup RaspberryPi
 def setup(pin: int):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
+
+
+# Create LCD
+def create_lcd(pin):
+
+    # Setup Display
+    PCF8574_address = 0x27
+    PCF8574A_address = 0x3F
+
+    # Create MCP
+    try:
+        mcp = PCF8574_GPIO(PCF8574_address)
+    except Exception:
+        try:
+            mcp = PCF8574_GPIO(PCF8574A_address)
+        except Exception as e:
+            raise e
+    lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+
+    # Turn on LCD
+    mcp.output(pin,1)
+    lcd.begin(16, 2)
+
+    # Return LCD
+    return lcd
 
 
 # Clean Up RaspberryPi
@@ -93,22 +108,82 @@ if __name__ == "__main__":
 
     # Parse User Arguments
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('pin', type=int, help='Physical number of GIPO pin')
+    parser.add_argument('pin_buzzer', type=int, help='Physical number of GPIO pin for buzzer')
+    parser.add_argument("pin_lcd", type=int, help="Physical number og GPIO pin for LCD")
     parser.add_argument('message', type=str, help='Message to morse')
     args = parser.parse_args()
-    pin = int(args.pin)
+    pin_buzzer = int(args.pin_buzzer)
+    pin_lcd = int(args.pin_lcd)
     msg = args.message
 
-    # Translate Message into Morse Code
-    msg_coded = translate(msg)
+    # Replace Umlauts in Message
+    msg = msg.replace("Ä", "Ae")
+    msg = msg.replace("ä", "ae")
+    msg = msg.replace("Ö", "Oe")
+    msg = msg.replace("ö", "oe")
+    msg = msg.replace("Ü", "Ue")
+    msg = msg.replace("ü", "ue")
+
+    # Check if Message is valid
+    morse_alphabet = set(morse_code_dict.keys()) 
+    if check_message(msg, morse_alphabet) is False:
+        raise ValueError("There are letters in your message not present in the Morse alphabet")
 
     # Configure RPi
-    setup(pin)
+    setup(pin_buzzer)
+
+    # Create LCD
+    lcd = create_lcd(pin_lcd)
 
     # Morse Message
     try:
-        play(pin, msg_coded)
+
+        # Set Initial Cursor Position
+        lcd.clear()
+        index = 0
+        lcd.setCursor(index, 0)
+       
+        # Split Message into Words
+        msg_split = re.split(r"\s+", msg)
+        
+        # Iterate through Message
+        for word in msg_split:
+
+            # Calculate Space Left
+            space_left = 32-index
+            if len(word) > space_left and len(word) < 32:
+                lcd.clear()
+                index = 0
+
+            # Play and Show Letter
+            for letter in word:
+
+                # Show Letter
+                if index >= 16:
+                    lcd.setCursor(index - 16,1)
+                if index > 31:
+                    lcd.clear()
+                    index = 0
+                    lcd.setCursor(index, 0)
+                lcd.message(letter)
+                index += 1
+
+                # Play Tone
+                morse_code = morse_code_dict[letter.upper()]
+                for tone in morse_code:
+                    play(pin_buzzer, tone)
+                sleep(0.3) # Pause between Sounds
+            
+            # Pause between Word
+            sleep(0.7)
+
+            # Print Space
+            if index != 16:
+                lcd.message(" ")
+                index += 1
+
     except Exception as e:
         raise e
     finally:
         destroy()
+        lcd.clear()
